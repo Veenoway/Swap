8571f14baf3d33b800c5192c5cb385a9756e5ec9
Veeno <sebastien.cloiseau@gmail.com>
2024-02-04 16:57:11 +0100

    feature: changeOwnerShip function

--- swap/aurelien/contracts/myContract.sol
+++ swap/aurelien/contracts/myContract.sol
@@ -4,6 +4,20 @@ pragma solidity ^0.8.19;
 contract CTR {
 
     uint256 public number = 5432;
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        require(owner == msg.sender, "Not the owner.");
+        _;
+    }
+
+    function changeOwnerShip(address _address) external onlyOwner {
+        owner = _address;
+    }
 
     function increment(uint256 _nbr) external {
         number += _nbr;
